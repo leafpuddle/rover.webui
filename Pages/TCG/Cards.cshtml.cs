4527bb1a1aba3f62dd7491f9db8df4ac9fbfc12f
Vi Roesler <vroesler@leafpuddle.com>
2022-02-15 14:02:34 -0600

    Adding cards page and individual card info page.

--- Pages/TCG/Cards.cshtml.cs
+++ Pages/TCG/Cards.cshtml.cs
@@ -5,15 +5,31 @@ namespace rover.webui.Pages;
 
 public class CardsModel : PageModel
 {
+    public ModelCard[] commonCards;
+    public ModelCard[] uncommonCards;
+    public ModelCard[] rareCards;
+    public ModelCard[] legendaryCards;
+    public ModelCard[] mythicCards;
+
     private readonly ILogger<IndexModel> _logger;
 
     public CardsModel(ILogger<IndexModel> logger)
     {
         _logger = logger;
+
+        commonCards = new ModelCard[0];
+        uncommonCards = new ModelCard[0];
+        rareCards = new ModelCard[0];
+        legendaryCards = new ModelCard[0];
+        mythicCards = new ModelCard[0];
     }
 
     public void OnGet()
     {
-
+        commonCards = QueryModelCards.GetCards(rarity: 1).Result;
+        uncommonCards = QueryModelCards.GetCards(rarity: 2).Result;
+        rareCards = QueryModelCards.GetCards(rarity: 3).Result;
+        legendaryCards = QueryModelCards.GetCards(rarity: 4).Result;
+        mythicCards = QueryModelCards.GetCards(rarity: 5).Result;
     }
 }
